2289c04055943d0b1d1d3aacc018ed4f36a3e570
Ansh Kumar Singal <anshkumarsinghal@gmail.com>
2024-07-30 03:01:29 +0530

    deleted

--- java script/index.js
+++
@@ -1,9 +0,0 @@
-let Price =[250, 645, 300, 900, 50];
-console.log(`before offer`,Price);
-
-for(let i=0 ; i<Price.length ; i++) {
-    let offer = Price[i]/10;
-    Price[i]= Price[i] - offer;
-}
-
-console.log(`after offer`, Price);
